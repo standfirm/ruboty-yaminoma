e671b541bcb92445b02fe89b09f2734a9209b90d
Pegasus204 <tenma.fukaya@misoca.jp>
2018-04-24 13:41:36 +0900

    反応する文字列を拡張した

--- lib/ruboty/handlers/yaminoma.rb
+++ lib/ruboty/handlers/yaminoma.rb
@@ -3,25 +3,25 @@
 module Ruboty
   module Handlers
     class Yaminoma < Base
-      on /あがります|かえります|おつかれさまです/i, name: 'yaminoma', description: 'やみのまリアクションをつける', all: true
+      on /あがります|(かえ|帰)ります|お(つか|疲)れさまです/i, name: 'yaminoma', description: 'やみのまリアクションをつける', all: true
 
       def yaminoma(message)
         message.add_reaction('yaminoma')
       end
 
-      on /おはよう/i, name: 'ohayo', description: 'sun_with_faceリアクションをつける', all: true
+      on /おは\w*|ohayo/i, name: 'ohayo', description: 'sun_with_faceリアクションをつける', all: true
 
       def ohayo(message)
         message.add_reaction('sun_with_face')
       end
 
-      on /おひる|ごはん/i, name: 'ohiru', description: 'rice_ballリアクションをつける', all: true
+      on /お(ひる|昼)|ごはん|lunch/i, name: 'ohiru', description: 'rice_ballリアクションをつける', all: true
 
       def ohiru(message)
         message.add_reaction('rice_ball')
       end
 
-      on /もどり/i, name: 'modori', description: 'backリアクションをつける', all: true
+      on /(もど|戻)り/i, name: 'modori', description: 'backリアクションをつける', all: true
 
       def modori(message)
         message.add_reaction('back')
